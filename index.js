d31e5ff03166263f329c5fa11d3fa7210995f488
Allison Sousa Bahls <allison_sousa_bahls@hotmail.com>
2019-07-03 15:45:57 -0400

    Single page aplication

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-var restify = require('restify');
+const restify = require('restify');
+const errs = require('restify-errors');
 
 const server = restify.createServer({
   name: 'myapp',
   version: '1.0.0'
 });
+
+const knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      user : 'root',
+      password : '',
+      database : 'db'
+    }
+  });
 
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
@@ -17,3 +28,69 @@ server.get('/echo/:name', function (req, res, next) {
 server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
+
+server.get('/', restify.plugins.serveStatic({
+ directory:'./dist',
+ file: 'index.html'
+}));
+
+
+
+server.get('/read', (req, res, next) => {
+    
+    knex('rest')
+    .then((dados)=>{
+        res.send(dados); 
+    },  next);
+   });
+
+server.get('/show/:id', function (req, res, next) {
+    const { id } = req.params;
+
+    knex('rest')
+    .where('id',id)
+    .first()
+    .then((dados)=>{
+        if(!dados) return res.send(new errs.BadRequestError("Nada foi encontrado"))
+        res.send(dados);
+    
+    },  next);
+   });
+
+  server.post('/create', function (req, res, next) {
+    
+    knex('rest')
+    .insert(req.body)
+    .then((dados)=>{
+        res.send(dados);
+    
+    },  next);
+    return next();
+  });
+
+
+  server.put('/update/:id', function (req, res, next) {
+    const { id } = req.params;
+
+    knex('rest')
+    .where('id',id)
+    .update(req.body)
+    .then((dados)=>{
+        if(!dados) return res.send(new errs.BadRequestError("Nada foi encontrado"))
+        res.send('dados atualizados');
+    
+    },  next);
+   });
+
+   server.del('/delete/:id', function (req, res, next) {
+    const { id } = req.params;
+
+    knex('rest')
+    .where('id',id)
+    .delete()
+    .then((dados)=>{
+        if(!dados) return res.send(new errs.BadRequestError("Nada foi encontrado"))
+        res.send('dados deletados');
+    
+    },  next);
+   });
